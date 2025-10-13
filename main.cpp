7c2efbc856edbb491269e34b86bd9d1f8cc808bc
3zymek <szymonpyrtek@gmail.com>
2025-10-14 00:13:40 +0200

    Working rendering for SFML

---
+++ main.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include "namespaces/Math.hpp"
+#include "core/engine/Engine.hpp"
+#include "core/managers/renderers/SFMLRenderer.hpp"
+#include "core/managers/renderers/IRenderer.hpp"
+int main()
+{
+	EngineConfig conf;
+	conf.setAPI(API::SFML);
+	//conf.setWindowName("test");
+	//conf.setWindowSize({ 1000, 1000 });
+
+	Engine engine(conf);
+	engine.init();
+	engine.run();
+}
