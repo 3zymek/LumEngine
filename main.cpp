29ef60cf3bbc1c01c39310382b57a1b9a4cce377
3zymek <szymonpyrtek@gmail.com>
2025-11-07 18:11:31 +0100

    working events, ecs and audio in progress

--- main.cpp
+++ main.cpp
@@ -1,60 +1,4 @@
-<<<<<<< HEAD
-﻿#include <iostream>
-#include "ev_manager.hpp"
-#include "ev_listener.hpp"
-
-// przykładowe eventy
-struct PlayerDamaged : public ev::detail::BaseEvent {
-    PlayerDamaged(int id, int dmg) : playerID(id), dmg(dmg) {}
-    int playerID{};
-    int dmg{};
-};
-
-struct FrameUpdate : public ev::detail::BaseEvent {
-    FrameUpdate(int f) : frame(f) {}
-    int frame{};
-};
-
-struct EnemySpawned : public ev::detail::BaseEvent {
-    EnemySpawned(std::string n, float x, float y) : name(n), x(x), y(y) {}
-    std::string name;
-    float x{}, y{};
-};
-
-int main()
-{
-    ev::EventManager em;
-
-    // test static subscribe
-    em.Subscribe<PlayerDamaged>([](const PlayerDamaged& e) {
-        std::cout << "[GLOBAL] Player " << e.playerID << " took " << e.dmg << "\n";
-        });
-
-    // test listener RAII
-    {
-        ev::EventListener lis(em);
-
-        // słuchacz dynamiczny
-        lis.Subscribe<FrameUpdate>([](const FrameUpdate& e) {
-            std::cout << "[LISTENER] frame " << e.frame << "\n";
-            });
-
-        // emitujemy
-        em.Emit<PlayerDamaged>({ 13, 5 });
-        em.Emit<FrameUpdate>({ 1 });
-        em.Emit<EnemySpawned>({ "Demon", 10,20 });
-        em.ProcessAll(); // listener żyje -> powinno wypisać
-    } // listener tu umiera -> unsubscribe auto
-
-    // ten FrameUpdate już NIE wywoła listenera
-    em.Emit<FrameUpdate>({ 2 });
-    em.Emit<PlayerDamaged>({ 13, 99 });
-    em.ProcessAll();
-
-    return 0;
-}
-=======
-﻿#include "ev_bus.hpp"
+#include "ev_bus.hpp"
 #include <iostream>
 #include <fmod.hpp>
 #include <e_define.hpp>
@@ -67,15 +11,7 @@ struct test {
     int t;
 };
 int main() {
-<<<<<<< HEAD
-
-
-
-}
->>>>>>> 312f60e (backup)
-=======
     Entity e;
     auto audio = e.AddComponent<AudioEmitterComponent>();
     audio->get().id = audio::AudioManager::Global().CreateEmitter();
 }
->>>>>>> 0c3c2bd (working events, ecs and audio in progress)
