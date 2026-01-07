1ffdd6e75194ca0d4f293b894b751382922d9a68
3zymek <szymonpyrtek@gmail.com>
2026-01-07 19:50:19 +0100

    handle pool patch

--- LumEngine/engine/include/core/utils/handle_pool.hpp
+++ LumEngine/engine/include/core/utils/handle_pool.hpp
@@ -12,7 +12,7 @@ namespace cstd {
 		using iterator = typename std::vector<DenseType>::iterator;
 		using const_iterator = typename std::vector<DenseType>::const_iterator;
 
-		Hsize_t NULL_HANDLE = -1;
+		static constexpr Hsize_t NULL_HANDLE = std::numeric_limits<Hsize_t>::max();
 
 	public:
 
@@ -31,11 +31,70 @@ namespace cstd {
 			return m_dense[m_sparse[handle.id]];
 		}
 
-		HandleType CreateHandle(const DenseType& obj);
+		HandleType CreateHandle(const DenseType& obj) {
+			if (m_dense.size() == MAX_SIZE)
+				throw std::runtime_error("Handle pool full");
 
-		void DeleteHandle(const HandleType& handle);
+			Slot slot;
 
-		void Reserve(Hsize_t new_max_size);
+			if (!m_free_slots.empty()) {
+				slot = m_free_slots.back();
+				m_free_slots.pop_back();
+			}
+			else
+				slot = nextSlot++;
+
+			m_dense.push_back(obj);
+
+			Hsize_t lastDense = m_dense.size() - 1;
+
+			m_sparse[slot] = lastDense;
+			m_dense_to_sparse[lastDense] = slot;
+
+			HandleType handle;
+			handle.id = slot;
+			handle.generation = m_generations[slot];
+
+			return handle;
+
+		}
+
+		void DeleteHandle(const HandleType& handle) {
+			Slot slot = handle.id;
+			if (slot >= m_sparse.size()) return;
+			if (m_generations[slot] != handle.generation) return;
+
+			Hsize_t denseIndex = m_sparse[slot];
+			Hsize_t lastIndex = m_dense.size() - 1;
+
+			if (denseIndex != lastIndex) {
+
+				std::swap(m_dense[denseIndex], m_dense[lastIndex]);
+
+				Slot movedSlot = m_dense_to_sparse[lastIndex];
+				m_sparse[movedSlot] = denseIndex;
+				m_dense_to_sparse[denseIndex] = movedSlot;
+
+
+			}
+
+			m_dense.pop_back();
+			m_dense_to_sparse.pop_back();
+
+			m_generations[slot]++;
+			m_free_slots.push_back(slot);
+
+			m_sparse[slot] = NULL_HANDLE;
+
+		}
+
+		void Reserve(Hsize_t new_max_size) {
+			m_dense.reserve(new_max_size);
+			m_sparse.resize(new_max_size, NULL_HANDLE);
+			m_dense_to_sparse.resize(new_max_size, NULL_HANDLE);
+			m_generations.resize(new_max_size, 0);
+			m_free_slots.reserve(new_max_size);
+		}
 
 		inline Hsize_t DenseSize() const { return m_dense.size(); }
 
@@ -64,5 +123,4 @@ namespace cstd {
 		std::vector<Slot>		m_free_slots;
 
 	};
-	#include "handle_pool.ipp"
 }
